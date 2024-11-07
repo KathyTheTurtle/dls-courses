7125b288974bada711606823a498e52f812c5911
KathyTheTurtle <scarf3blue@gmail.com>
2024-11-07 10:15:01 +0700

    Change vh to dvh to account for mobile

--- src/components/RootLayout/RootLayout.tsx
+++ src/components/RootLayout/RootLayout.tsx
@@ -3,7 +3,7 @@ import { Center } from '@mantine/core';
 
 export function RootLayout() {
   return (
-    <Center style={{ height: '100vh', width: '100vw' }}>
+    <Center style={{ height: '100dvh', width: '100vw' }}>
       <Outlet />
     </Center>
   );
